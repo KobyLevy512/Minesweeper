84dda8c3f31698061f5fdaaa1904efc6fbde838c
Koby Levy <86796339+KobyLevy512@users.noreply.github.com>
2022-01-28 00:09:01 +0200

    Add files via upload

---
+++ Minesweeper/Minesweeper/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        const int WIDHT = 617;
+        const int HEIGHT = 480;
+        public MainMenu menuRef;
+        public int Level;
+        public bool GoToMenu = false;
+        public bool FirstInit = true;
+        private int Score = 0;
+        GameButton[,] gameButtons = new GameButton[HEIGHT / 40 - 1, WIDHT / 40];
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = WIDHT;
+            this.Height = HEIGHT;
+        }
+        public void InitGame()
+        {
+            if (!FirstInit) MessageBox.Show("Score:" + Score);
+            else FirstInit = false;
+            Score = 0;
+            Random rnd = new Random();
+            this.Controls.Clear();
+            int fixLevel = Level == 0?3: Level == 1?4:6;
+            for(int y = 0; y< HEIGHT / 40 - 1; y++)
+               for (int x = 0; x < WIDHT / 40; x++)
+               {
+                    int tmp = 0;
+                    int rand = rnd.Next(0, 1000);
+                    if (rand > 950 - Level * 50 && rand < 990) tmp = 0;
+                    else tmp = rnd.Next(1, fixLevel);
+                    rand -= 1;
+                    gameButtons[y,x] = new GameButton(x * 40, y * 40, (Value)tmp, new Point(x,y));
+                    this.Controls.Add(gameButtons[y, x].GetButton);
+                    gameButtons[y, x].Lost += InitGame;
+                    gameButtons[y, x].Open += OpenButton;
+                    gameButtons[y, x].Score += PlayerScore;
+                    gameButtons[y, x].GetButton.KeyDown += Form1_KeyDown;
+               }
+        }
+
+        private void OpenButton(Point index, Value value)
+        {
+            int i = 1;
+            if (index.X + i < gameButtons.GetLength(1) && value == gameButtons[index.Y, index.X + i].value && gameButtons[index.Y, index.X + i].state != State.Open)
+            {
+                gameButtons[index.Y, index.X + i].OpenButton();
+            }
+            if (index.X - i >= 0 && value == gameButtons[index.Y, index.X - i].value && gameButtons[index.Y, index.X - i].state != State.Open)
+            {
+                gameButtons[index.Y, index.X - i].OpenButton();
+            }
+
+            if (index.Y + i < gameButtons.GetLength(0) && value == gameButtons[index.Y + i, index.X].value && gameButtons[index.Y + i, index.X].state != State.Open)
+            {
+                gameButtons[index.Y + i, index.X].OpenButton();
+            }
+            if (index.Y - i >= 0 && value == gameButtons[index.Y - i, index.X].value && gameButtons[index.Y - i, index.X].state != State.Open)
+            {
+                gameButtons[index.Y - i, index.X].OpenButton();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(!GoToMenu) menuRef.Close();
+        }
+        private void PlayerScore()
+        {
+            Score++;
+            if (Score >= 165)
+            {
+                MessageBox.Show("You WIN !!");
+                menuRef.Show();
+                GoToMenu = true;
+                this.Close();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                DialogResult dr = MessageBox.Show("Back to Main Menu are you sure?", "Exit", MessageBoxButtons.OKCancel);
+                if(dr == DialogResult.OK)
+                {
+                    menuRef.Show();
+                    GoToMenu = true;
+                    this.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper/GameButton.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public enum State
+    {
+        Null,
+        Open,
+        Mine
+    }
+    public enum Value
+    {
+        Mine = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4
+    }
+    public class GameButton
+    {
+        public delegate void PlayerLost();
+        public delegate void PlayerScore();
+        public delegate void PlayerOpen(Point Index, Value state);
+        public event PlayerLost Lost;
+        public event PlayerOpen Open;
+        public event PlayerScore Score;
+        Button button = new Button();
+        public State state = State.Null;
+        public Value value;
+
+        public SoundPlayer sound;
+        public Point Index;
+        public Button GetButton
+        {
+            get => button;
+        }
+
+        public GameButton(int x, int y, Value val, Point index)
+        {
+            this.Index = index;
+            button.Location = new Point(x, y);
+            button.Width = 40;
+            button.Height = 40;
+            button.BackColor = Color.FromArgb(200, 200, 200);
+            button.ForeColor = Color.White;
+            button.Font = new Font(FontFamily.GenericSansSerif, 16);
+            button.BackgroundImageLayout = ImageLayout.Stretch;
+            button.MouseDown += button_Click;
+            Random rnd = new Random();
+            value = val;
+        }
+        private void button_Click(object sender, MouseEventArgs e)
+        {
+            if(state != State.Open)
+            {
+                switch (e.Button)
+                {
+                    case MouseButtons.Left:
+                        if (value == Value.Mine) OpenMineButton();
+                        else OpenButton();
+                        break;
+                    case MouseButtons.Right:
+                        if(state == State.Mine)
+                        {
+                            state = State.Null;
+                            button.BackgroundImage = null;
+                        }
+                        else
+                        {
+                            if(value == Value.Mine)Score?.Invoke();
+                            state = State.Mine;
+                            button.BackgroundImage = Properties.Resources.Flag;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void OpenMineButton()
+        {
+            state = State.Open;
+            button.BackColor = Color.FromArgb(200, 0, 0);
+            button.BackgroundImage = Properties.Resources.Bomb;
+            sound = new SoundPlayer(Properties.Resources.RobotSound);
+            sound.Play();
+            MessageBox.Show("You Lost");
+            Lost?.Invoke();
+        }
+        public void OpenButton()
+        {
+            state = State.Open;
+            if (sound != null)sound.Stop();
+            sound = new SoundPlayer(Properties.Resources.WinFx);
+            sound.Play();
+            button.BackColor = Color.FromArgb(50, 205, 75);
+            button.BackgroundImage = null;
+            button.Text = ((int)value).ToString();
+            Open?.Invoke(Index, value);
+            Score?.Invoke();
+
+        }
+    }
+}
